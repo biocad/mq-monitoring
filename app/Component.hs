8a496bc95d96492373dcbed3515996410f03d5da
AlexKaneRUS <AlexKaneRUS@gmail.com>
2018-05-15 16:23:43 +0300

    Version 0.1.0.1. HOT FIX

--- app/Component.hs
+++ app/Component.hs
@@ -2,8 +2,8 @@
 
 module Main where
 
-import           System.MQ.Component.App (runAppWithTech)
-import           System.MQ.Monitoring    (monitoringAction)
+import           System.MQ.Component  (runAppWithTech)
+import           System.MQ.Monitoring (monitoringAction)
 
 main :: IO ()
 main = runAppWithTech "mq_monitoring" (const $ return ()) monitoringAction
