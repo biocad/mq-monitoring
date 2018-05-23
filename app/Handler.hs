422abfdd8f535523e6b30dcfc0ec05f8c1a55c06
Alexander <AlexKaneRUS@gmail.com>
2018-05-23 12:26:46 +0300

    Version 0.1.1.2. Added option to handler for getting errors from MQ (#4)

--- app/Handler.hs
+++ app/Handler.hs
@@ -1,63 +1,76 @@
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import           Control.Monad.IO.Class       (liftIO)
-import           Data.Aeson                   (FromJSON (..), ToJSON (..),
-                                               genericParseJSON, object, (.=))
-import           Data.Aeson.Casing            (aesonPrefix, snakeCase)
-import           Data.Aeson.Picker            ((|-?))
-import           Data.Bson                    (Document, (=:))
-import           Data.Either                  (rights)
-import           Data.Function                (on)
-import           Data.List                    (groupBy, sortOn)
-import qualified Data.Map.Strict              as M (fromList, keys, (!?))
-import           Data.Maybe                   (fromMaybe)
-import           Data.Text.Lazy               (Text, toStrict, unpack)
-import           Database.MongoDB.WrapperNew  (MongoPool, decode, find,
-                                               loadMongoPool, withMongoPool)
-import           GHC.Generics                 (Generic)
-import           System.BCD.Config            (getConfigText)
-import           System.MQ.Monad              (runMQMonad)
-import           System.MQ.Monitoring         (monitoringColl)
-import           System.MQ.Protocol           (Timestamp, getTimeMillis)
-import           System.MQ.Protocol.Technical (MonitoringData (..))
-import           Web.Scotty.Trans             (get, json, params)
-import           Web.Template                 (CustomWebServer (..),
-                                               Process (..), Route (..),
-                                               runWebServer)
+import           Control.Monad.IO.Class      (liftIO)
+import           Data.Aeson                  (FromJSON (..), ToJSON (..))
+import           Data.Aeson.Picker           ((|-?))
+import           Data.Bson                   (Document, (=:))
+import           Data.Either                 (rights)
+import           Data.Function               (on)
+import           Data.List                   (groupBy, sortOn)
+import qualified Data.Map.Strict             as M (fromList, keys, (!?))
+import           Data.Maybe                  (fromMaybe)
+import qualified Data.Text                   as T (Text)
+import           Data.Text.Lazy              (Text, toStrict, unpack)
+import           Database.MongoDB.WrapperNew (MongoPool, decode, find,
+                                              loadMongoPool, withMongoPool)
+import           System.BCD.Config           (getConfigText)
+import           System.MQ.Monad             (runMQMonad)
+import           System.MQ.Monitoring        (ErrorDBUnit (..),
+                                              MoniUserData (..), errorsColl,
+                                              monitoringColl, toUser)
+import           System.MQ.Protocol          (Timestamp, getTimeMillis)
+import           Web.Scotty.Trans            (get, json, params)
+import           Web.Template                (CustomWebServer (..),
+                                              Process (..), Route (..),
+                                              runWebServer)
+
+-- | Type of handler that is being run
+--
+data Handler a b = Handler { name          :: b -> String -- ^ getter of name from db unit
+                           , time          :: b -> Int    -- ^ getter of time from db unit
+                           , changeMessage :: a -> b      -- ^ function to change representation of db unit
+                           , collName      :: T.Text      -- ^ collection in which units are stored
+                           }
 
 main :: IO ()
 main = do
     port <- portIO
     runWebServer port monitoringServer
   where
-    monitoringServer = CustomWebServer () [Route get 1 "/monitoring" handlerMonitoring]
+    monitoringServer = CustomWebServer () [ Route get 1 "/monitoring" $ runHandler monitoringHandler
+                                          , Route get 1 "/errors" $ runHandler errorsHandler
+                                          ]
+
+    monitoringHandler = Handler muName muSyncTime toUser monitoringColl
+    errorsHandler = Handler erName erSyncTime id errorsColl
 
     portIO :: IO Int
     portIO = do
         config <- getConfigText
         return $ fromMaybe 3000 $ config |-? ["params", "mq_monitoring_handler", "port"]
 
-handlerMonitoring :: Process ()
-handlerMonitoring = Process $ do
-  pool <- liftIO $ loadMongoPool "mq-monitoring-handler"
-  paramMap <- fmap M.fromList params
+runHandler :: forall a b . (FromJSON a, ToJSON b) => Handler a b -> Process ()
+runHandler Handler{..} = Process $ do
+    pool <- liftIO $ loadMongoPool "mq-monitoring-handler"
+    paramMap <- fmap M.fromList params
 
-  messages <- liftIO $ handleReq pool (paramMap M.!? "name") (paramMap M.!? "since")
+    messages <- liftIO $ handleReq pool (paramMap M.!? "name") (paramMap M.!? "since")
+    let changedMessages = fmap changeMessage messages
 
-  if "last" `elem` M.keys paramMap
-    then json $ lastMessages messages
-    else json messages
+    if "last" `elem` M.keys paramMap
+      then json $ lastMessages changedMessages
+      else json changedMessages
 
   where
-    handleReq :: MongoPool -> Maybe Text -> Maybe Text -> IO [MoniUserData]
+    handleReq :: MongoPool -> Maybe Text -> Maybe Text -> IO [a]
     handleReq pool specM sinceM = do
         query <- formQuery specM sinceM
-        mData <- fmap (rights . fmap decode) . withMongoPool pool $ find 0 monitoringColl query
-        return $ toUser <$> mData
+        mData <- fmap (rights . fmap decode) . withMongoPool pool $ find 0 collName query
+        return mData
 
     formQuery :: Maybe Text -> Maybe Text -> IO Document
     formQuery specM sinceM = do
@@ -70,29 +83,12 @@ handlerMonitoring = Process $ do
 
         return $ specQuery ++ sinceQuery
 
-    lastMessages :: [MoniUserData] -> [MoniUserData]
-    lastMessages = fmap (head . reverse . sortOn muSyncTime) . groupBy ((==) `on` muName) . sortOn muName
+    lastMessages :: [b] -> [b]
+    lastMessages mData = res
+      where
+        groupedData = groupBy ((==) `on` name) . sortOn name $ mData
+
+        res = fmap (head . reverse . sortOn time) groupedData
 
     oneDay :: Timestamp
     oneDay = 86400000
-
--- | Format in which data is returned to user.
---
-data MoniUserData = MoniUserData { muSyncTime :: Timestamp
-                                 , muName     :: String
-                                 , muIsAlive  :: Bool
-                                 , muMessage  :: String
-                                 }
- deriving (Eq, Show, Generic)
-
-instance ToJSON MoniUserData where
- toJSON MoniUserData{..} = object $ [ "sync_time" .= muSyncTime
-                                    , "name" .= muName
-                                    , "is_alive" .= muIsAlive
-                                    ] ++ if null muMessage then [] else [ "message" .= muMessage ]
-
-instance FromJSON MoniUserData where
- parseJSON = genericParseJSON $ aesonPrefix snakeCase
-
-toUser :: MonitoringData -> MoniUserData
-toUser MonitoringData{..} = MoniUserData mSyncTime mName mIsAlive mMessage
--- src/System/MQ/Monitoring.hs
+++ src/System/MQ/Monitoring.hs
@@ -5,38 +5,82 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module System.MQ.Monitoring
-  ( monitoringAction
+  ( ErrorDBUnit (..)
+  , MoniUserData (..)
+  , errorsColl
+  , monitoringActionComm
+  , monitoringActionTech
   , monitoringColl
+  , toUser
   ) where
 
-import           Control.Concurrent           (forkIO)
-import           Control.Monad                (when)
-import           Control.Monad.Except         (catchError, liftIO)
-import           Data.String                  (fromString)
-import           Data.Text                    (Text)
-import           Database.MongoDB             (insert_)
-import           Database.MongoDB.WrapperNew  (MongoPool, encode, loadMongoPool,
-                                               withMongoPool)
-import           System.MQ.Component          (Env (..), TwoChannels (..),
-                                               loadTechChannels, runTech)
-import           System.MQ.Monad              (MQMonad, errorHandler,
-                                               foreverSafe, runMQMonad)
-import           System.MQ.Protocol           (Condition (..), Message (..),
-                                               MessageLike (..), MessageTag,
-                                               Props (..), matches, messageSpec,
-                                               messageType)
-import           System.MQ.Protocol.Technical (MonitoringData)
-import           System.MQ.Transport          (sub)
+import           Control.Concurrent                  (forkIO)
+import           Control.Monad                       (when)
+import           Control.Monad.Except                (catchError, liftIO)
+import           Data.Aeson                          (ToJSON)
+import           Data.String                         (fromString)
+import           Data.Text                           (Text)
+import           Database.MongoDB                    (insert_)
+import           Database.MongoDB.WrapperNew         (MongoPool, encode,
+                                                      loadMongoPool,
+                                                      withMongoPool)
+import           System.MQ.Component                 (Env (..),
+                                                      TwoChannels (..),
+                                                      load2Channels,
+                                                      loadTechChannels, runTech)
+import           System.MQ.Error                     (MQError (..))
+import           System.MQ.Monad                     (MQMonad, errorHandler,
+                                                      foreverSafe, runMQMonad)
+import           System.MQ.Monitoring.Internal.Types (ErrorDBUnit (..),
+                                                      MoniUserData (..), toUser)
+import           System.MQ.Protocol                  (Condition (..), Creator,
+                                                      Message (..),
+                                                      MessageLike (..),
+                                                      MessageTag,
+                                                      MessageType (..),
+                                                      Props (..), matches,
+                                                      messageCreator,
+                                                      messageSpec, messageType)
+import           System.MQ.Protocol.Technical        (MonitoringData)
+import           System.MQ.Transport                 (sub)
 
 -- | Name of collection where monitoring data is stored
 --
 monitoringColl :: Text
 monitoringColl = "monitoring"
 
+-- | Name of collection where error data is stored
+--
+errorsColl :: Text
+errorsColl = "errors"
+
+-- | Action for communication level that performs monitoring of Monique
+--
+monitoringActionComm :: Env -> MQMonad ()
+monitoringActionComm Env{..} = do
+    TwoChannels{..} <- load2Channels
+    pool <- liftIO $ loadMongoPool "mq-monitoring"
+
+    foreverSafe name $ do
+      (tag, msg) <- sub fromScheduler
+
+      when (filterTag tag) $ processErrorData pool (messageCreator tag) msg
+
+  where
+    Props{..} = props :: Props MQError
+
+    filterTag :: MessageTag -> Bool
+    filterTag = (`matches` (messageType :== mtype :&& messageSpec :== fromString spec))
+
+    processErrorData :: MongoPool -> Creator -> Message -> MQMonad ()
+    processErrorData pool mCreator Message{..} = do
+        MQError{..} <- unpackM msgData
+        storeDataInDB pool errorsColl $ ErrorDBUnit errorCode errorMessage msgCreatedAt mCreator
+
 -- | Action for techincal level that performs monitoring of Monique
 --
-monitoringAction :: Env -> MQMonad ()
-monitoringAction env@Env{..} = do
+monitoringActionTech :: Env -> MQMonad ()
+monitoringActionTech env@Env{..} = do
     _ <- liftIO $ forkIO $ catchMQ $ listenerTech env
     runTech env
   where
@@ -49,18 +93,18 @@ listenerTech Env{..} = do
     pool <- liftIO $ loadMongoPool "mq-monitoring"
 
     foreverSafe name $ do
-      (tag, Message{..}) <- sub fromScheduler
+      (tag, msg) <- sub fromScheduler
 
-      when (filterTag tag) $ do
-        mData <- unpackM msgData
-        storeDataInDB pool mData
+      when (filterTag tag) $ processMoniData pool msg
 
   where
     Props{..} = props :: Props MonitoringData
 
     filterTag :: MessageTag -> Bool
-    filterTag = (`matches` (messageType :== mtype :&& messageSpec :== fromString spec))
+    filterTag = (`matches` ((messageType :== mtype :&& messageSpec :== fromString spec) :|| messageType :== Error))
 
-storeDataInDB :: MongoPool -> MonitoringData -> MQMonad ()
-storeDataInDB pool = liftIO . withMongoPool pool . insert_ monitoringColl . encode
+    processMoniData :: MongoPool -> Message -> MQMonad ()
+    processMoniData pool Message{..} = (unpackM msgData :: MQMonad MonitoringData) >>= storeDataInDB pool monitoringColl
 
+storeDataInDB :: ToJSON a => MongoPool -> Text -> a -> MQMonad ()
+storeDataInDB pool collName = liftIO . withMongoPool pool . insert_ collName . encode
